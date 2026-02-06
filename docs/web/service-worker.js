aa4b7a1b1eac01dadc0974245760cf68a1144e90
José Luis <21303541+joseluisct@users.noreply.github.com>
2026-02-06 18:11:19 +0100

    Bug fixed

--- docs/web/service-worker.js
+++ docs/web/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: EGRTg+ja */
+/* Manifest version: 5rDtTccK */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
